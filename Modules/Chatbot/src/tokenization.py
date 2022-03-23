d4300002ad34a3c9454aa67db64e30d34fc822a6
AhmedMGZ7 <48853566+AhmedMGZ7@users.noreply.github.com>
2022-03-23 19:42:33 +0200

    Update tokenization.py

--- Modules/Chatbot/src/tokenization.py
+++ Modules/Chatbot/src/tokenization.py
@@ -1,14 +1,7 @@
 import re
 def get_tokens(text):
-    punct = {',','.','!',':','؟','?','/','\\','-','_','=','(',')','{','}','[',']','#','+','<','>','\'','\"'}
     dictionary = {'د':"دكتور"}#to be continued
-    w = " "
-    for char in text:
-        if char not in punct:
-            w += char
-        else:
-            w += " "
-    words_after_split = w.split()
+    words_after_split = text.split()
     for index , word in enumerate(words_after_split):
         if word in dictionary.keys():
             w = dictionary[word]
