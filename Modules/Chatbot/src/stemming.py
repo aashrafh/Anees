183b2c1afe1c229b1aa13c95922afe007fd29f91
ahmedsherif304 <ahmedsherifelminshawy@gmail.com>
2022-04-21 18:18:51 +0200

    adding nouns stemming

--- Modules/Chatbot/src/stemming.py
+++ Modules/Chatbot/src/stemming.py
@@ -1,6 +1,29 @@
-# prefix = {}
-# sufix = {}
-# def stemm(tokens_verb_noun):
-#     for item in tokens_verb_noun:
-#         if item[1] == 'n':
+noun_prefix = {'ال'}
+noun_suffix = {'كن','كما','كم','هم','هن','هما'}
+def stem(tokens_verb_noun):
+    tokens_stemmed = list()
+    for token in tokens_verb_noun:
+        if token[1] == 'v':
+            tokens_stemmed.append(token)
+            continue
+        noun = token[0]
+        noun_length = len(noun)
+        done = 0
+        for prefix in noun_prefix:
+            if noun_length - len(prefix) > 2 and noun[:len(prefix)] == prefix:
+                tokens_stemmed.append(( noun[len(prefix):] ,'n'))
+                done = 1
+                break
+        if not done:
+            for suffix in noun_suffix:
+                if noun_length - len(suffix) > 2 and noun[noun_length - len(suffix):] == suffix:
+                    tokens_stemmed.append(( noun[0 : noun_length - len(suffix)] ,'n'))
+                    done = 1
+                    break
+        if not done:
+            tokens_stemmed.append(token)
+    return tokens_stemmed
+
+            
+                
             
