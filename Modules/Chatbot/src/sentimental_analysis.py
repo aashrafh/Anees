66fcee84f5f95f669aa0777746ba9c41f8d04137
AhmedMGZ7 <omagdy837@yahoo.com>
2022-05-17 22:57:54 +0200

    final sentimental analysis
    pos and neg accuracy 80% and all emotions 67%

--- Modules/Chatbot/src/sentimental_analysis.py
+++ Modules/Chatbot/src/sentimental_analysis.py
@@ -2,11 +2,24 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 
 def get_emotion(text):
-    filename = f'../utils/sentmental_model.sav'
+    filename = f'../utils/sentmental_+-_model.sav'
     model = pickle.load(open(filename, 'rb'))
-    filename = f'../utils/tfidf_model.sav'
+    filename = f'../utils/tfidf_+-_model.sav'
     tf_idf = pickle.load(open(filename, 'rb'))
-    text = tf_idf.transform([text]).toarray()
-    emotion = model.predict(text)
-    return emotion[0]
+    text0 = tf_idf.transform([text])
+    pos_neg = model.predict(text0)[0]
+    filename = f'../utils/sentmental_pos_model.sav'
+    model_pos = pickle.load(open(filename, 'rb'))
+    filename = f'../utils/tfidf_pos_model.sav'
+    tf_idf_pos = pickle.load(open(filename, 'rb'))
+    filename = f'../utils/sentmental_neg_model.sav'
+    model_neg = pickle.load(open(filename, 'rb'))
+    filename = f'../utils/tfidf_neg_model.sav'
+    tf_idf_neg = pickle.load(open(filename, 'rb'))
+    if pos_neg == 'pos':
+        tweet = tf_idf_pos.transform([text])
+        return model_pos.predict(tweet)[0]
+    else:
+        tweet = tf_idf_neg.transform([text])
+        return model_neg.predict(tweet)[0]
     
--- Modules/Chatbot/src/stemming.py
+++ Modules/Chatbot/src/stemming.py
@@ -1,4 +1,4 @@
-noun_prefix = {'ال'}
+noun_prefix = {'ال','ب','ك','بال','كال','لل'}
 noun_suffix = {'كن','كما','كم','هم','هن','هما'}
 def stem(tokens_verb_noun):
     tokens_stemmed = list()
