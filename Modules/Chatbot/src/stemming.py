5ea4d1c8ecec04955ccde5e02b1d592ce96af7ec
AhmedMGZ7 <omagdy837@yahoo.com>
2022-06-30 14:41:49 +0200

    base of weather task
    added data for countries and their corresponding name in Arabic and their capital to be used in the weather api which requires city if the text doesnot have a city but a country

--- Modules/Chatbot/src/stemming.py
+++ Modules/Chatbot/src/stemming.py
@@ -3,7 +3,7 @@ noun_suffix = {'كن','كما','كم','هم','هن','هما'}
 def stem(tokens_verb_noun):
     tokens_stemmed = list()
     for token in tokens_verb_noun:
-        if token[1] == 'v':
+        if token[1] != 'n':
             tokens_stemmed.append(token)
             continue
         noun = token[0]
