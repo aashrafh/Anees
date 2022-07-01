b0ab1115205520e857d10187b30c5af2ae8f0c25
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2022-07-01 15:35:26 +0200

    Modify interact script

--- Modules/Chatbot/src/Generation/interact.sh
+++ Modules/Chatbot/src/Generation/interact.sh
@@ -1,15 +1,13 @@
 python src/main.py \
-    --seed=0 \
     --mode="infer" \
     --data_dir="/content/drive/MyDrive/GP/gpt2/data" \
     --model_type="aubmindlab/aragpt2-base" \
     --bos_token="<bos>" \
     --sp1_token="<sp1>" \
     --sp2_token="<sp2>" \
-    --gpu="0" \
     --max_len=256 \
     --max_turns=5 \
     --top_p=0.8 \
     --ckpt_dir="/content/drive/MyDrive/GP/gpt2/saved_models" \
     --ckpt_name="best_ckpt_epoch=1_valid_loss=1.0759" \
-    --end_command="Abort!"
+    --end_command="EOC!"
