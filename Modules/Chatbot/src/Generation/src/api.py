af8adddf18253bca03ca8ac6e87e5adce035e10a
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2022-07-12 17:12:17 +0200

    Create an API for the generation

---
+++ Modules/Chatbot/src/Generation/src/api.py
@@ -0,0 +1,63 @@
+import uvicorn
+from pyngrok import ngrok
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from main import load_arz_model, load_msa_model
+
+
+BASE_PATH = '/content/drive/MyDrive/anees'
+
+print("Loading the ARZ model...")
+arz = load_arz_model(f'{BASE_PATH}/data', f'{BASE_PATH}/chats_monsoon_ckpts',
+                     'best_ckpt_epoch=2_valid_loss=4.4047')
+
+print("Loading the MSA model...")
+msa = load_msa_model(f'{BASE_PATH}/data', f'{BASE_PATH}/translated_aragpt2_ckpts',
+                     'best_ckpt_epoch=9_valid_loss=3.2331')
+
+print("Setting up the API...")
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+@app.get("/")
+def hello():
+    return {
+        'response': "مرحبا، انا انيس"
+    }
+
+
+@app.post("/arz")
+def arz_response(utter, history):
+    resp = ''
+    try:
+        resp = arz.respond(utter, history)
+    except:
+        resp = 'انا مش قادر افهمك للاسف'
+    return {
+        'response': resp
+    }
+
+
+@app.post("/msa")
+def msa_response(utter, history):
+    resp = ''
+    try:
+        resp = msa.respond(utter, history)
+    except:
+        resp = 'لا استطيع فهمك. انا اسف'
+    return {
+        'response': resp
+    }
+
+
+ngrok_tunnel = ngrok.connect(8000)
+print('Public URL:', ngrok_tunnel.public_url)
+uvicorn.run(app, port=8000)
