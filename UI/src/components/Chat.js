f34686ed4f6d8370a3cad2475a232ec9c7c19788
ahmedsherif304 <ahmedsherifelminshawy@gmail.com>
2022-07-13 19:56:32 +0200

    retreiving the messages in the front end

--- UI/src/components/Chat.js
+++ UI/src/components/Chat.js
@@ -14,8 +14,8 @@ import { api } from "../api";
 //     avatar: "https://placeimg.com/140/140/any",
 //   },
 // },
-
 const Chat = () => {
+  const aneesAvatar = "https://placeimg.com/140/140/any"
   const [messages, setMessages] = useState([]);
 
   const renderBubble = (props) => {
@@ -48,34 +48,40 @@ const Chat = () => {
   }, []);
 
   useEffect(() => {
+    api.post("/history",{username : "Ahmed"}).then((res)=>{
+      const history = res.data.response
+      setMessages(history.reverse())
+    }).catch((err)=>{console.log(err)})
+  },[])
+
+  useEffect(() => {
     if (messages.length > 0 && messages[0]?.user?._id === 1) {
       api
-        .get("/getResponse", {
-          data: {
+        .post("/getResponse", {
             username: "Ahmed",
-            text: messages[0].text,
-          },
+            text: messages[0].text
         })
         .then((res) => {
           console.log(res);
+            setMessages((previousMessages) =>
+              GiftedChat.append(previousMessages, [
+                {
+                  _id: messages.length + 1,
+                  text: res.data.response.text,
+                  createdAt: new Date(),
+                  user: {
+                    _id: 0,
+                    name: "أنيس",
+                    avatar: aneesAvatar,
+                  },
+                },
+              ])
+            );
         })
         .catch((err) => {
           console.log(err);
         });
-      // setMessages((previousMessages) =>
-      //   GiftedChat.append(previousMessages, [
-      //     {
-      //       _id: messages.length + 1,
-      //       text: "Recieved!",
-      //       createdAt: new Date(),
-      //       user: {
-      //         _id: 2,
-      //         name: "React Native",
-      //         avatar: "https://placeimg.com/140/140/any",
-      //       },
-      //     },
-      //   ])
-      // );
+
     }
   }, [messages]);
 
