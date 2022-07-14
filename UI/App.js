154f1ec682b3caf774a266b7c75cacf768170c98
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2022-07-14 02:59:57 +0200

    Signup screen

--- UI/App.js
+++ UI/App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Chat from "./src/screens/Chat";
 import Login from "./src/screens/Login";
+import Signup from "./src/screens/Signup";
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Chat" component={Chat} />
       </Stack.Navigator>
     </NavigationContainer>
