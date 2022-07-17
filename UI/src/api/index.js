90373bc162d1b515812a53b83a8519d5b8cd657b
AhmedMGZ7 <omagdy837@yahoo.com>
2022-07-17 17:46:47 +0200

    url

--- UI/src/api/index.js
+++ UI/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://1dc3-197-52-128-201.eu.ngrok.io";
+const API_URL = "https://83db-102-44-194-191.eu.ngrok.io/";
 
 const headers = {
   Accept: `application/json`,
