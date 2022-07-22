0fbefaa5c9199811d5ba0a714cc45ccaeb54cef8
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2022-07-22 16:00:54 +0200

    Update requirements.txt

--- modules/config.py
+++ modules/config.py
@@ -1,8 +1,8 @@
 MONGO_URI = "mongodb://mongodb/Anees"
 GENERATION_API = 'http://generation:8000/msa'
-WEATHER_API_KEY = "acc2137340c0aeb19d563cf85cf78595"
-MAP_API_KEY = "AIzaSyDKR0mQtI6fh1bmcaMcdkWqGv0UuoZ7_uA"
-CSE_ID = "32ccd6e123a6d49d7"
+WEATHER_API_KEY = "<YOUR_WEATHER_API_KEY>"
+MAP_API_KEY = "<YOUR_MAP_API_KEY>"
+CSE_ID = "<YOUR_CSE_ID>"
 
 STOPWORDS_PATH = 'data/stopwords.txt'
 VERB_DICTIONAY_PATH = 'data/verb_dictionary.txt'
